d083833409f233b4b04bdf01d0e4f514c2eaaca5
Jackson Liu <416883813@qq.com>
2018-07-01 11:08:12 +0800

    add assets

--- main.go
+++ main.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-	"net/http"
     "github.com/labstack/echo"
     "github.com/labstack/echo/middleware"
     // "github.com/jinzhu/gorm"
     model "goBlog/model"
 )
-
-type Person struct {
-    ID uint `json:”id”`
-    FirstName string `json:”firstname”`
-    LastName string `json:”lastname”`
-}
 
 func main() {
     e := echo.New()
@@ -20,26 +13,10 @@ func main() {
     e.Use(middleware.Logger())
     e.Use(middleware.Recover())
 
-    // 连接数据库
-    // if db.HasTable(&Person{}) == false {
-    //     db.CreateTable(&Person{})
-    // }
-    //
-
     model.DB()
 
-    // db := model.DB()
-
-    // db.Create(&model.User{
-    //     Name: "main",
-    //     Password: "asdddd",
-    //     Info: "is root",
-    // })
-
-    e.GET("/", func(c echo.Context) error {
-        return c.String(http.StatusOK, "Hello world \n")
-    })
+    // 静态资源
+    e.Static("/assets", "./assets")
 
     e.Logger.Fatal(e.Start(":1234"))
-
 }
