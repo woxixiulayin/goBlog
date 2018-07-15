c2ef5c9d75669c8d55fce3646869f2bc79cee3b1
Jackson Liu <416883813@qq.com>
2018-07-15 15:47:03 +0800

    add bindRxObservable

--- assets/src/views/App/Body.jsx
+++ assets/src/views/App/Body.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Module, { bindRxObservable } from 'src/lib/Module'
+
+const todos = new Module({
+    state: {
+    }
+})
+
+window.todos = todos
+@bindRxObservable(todos.$$state, {
+    todo: (state, ownProps) => state[ownProps.id] || {}
+})
+class Todos extends Component {
+    render() {
+        const { todo = {} } = this.props
+        return <div>
+        {todo.title}
+        </div>
+    }
+}
 
 const Body = props => <section id="body">
     <div className="content">
+        <Todos id={1} />
         {props.children || ''}
     </div>
 </section>
